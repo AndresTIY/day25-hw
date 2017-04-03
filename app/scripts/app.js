ec50707b421f71005c4718f6e03b5013b11b12b6
Andres Cifuentes <aacifuentes@gmail.com>
2017-04-03 15:44:32 -0500

    basic javascript info entered, initialState defined

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,32 @@
+import { createStore } from 'redux'
 
 export default function app() {
+  const url = 'http://tiny-za-server.herokuapp.com/collections/dres-cafe'
+  const apiUrl = 'https://tiy-austin-front-end-engineering.github.io/restaurantApi/fancy.json';
 
-    console.log(Redux.store('foo'));
-}
+  const initialState = {
+    cart: {},
+    subtotal: null,
+    tax: 1.08,
+    finalTotal: null,
+    view: menuView
+  }
+
+  const reducer = function (currentState, action){
+    if (currentState === undefined){
+      return initialState;
+    }
+
+
+  }//end of reducer
+  const store = createStore(reducer, initialState);
+
+  const render = function(){
+    let state = store.getState();
+    $('#app').html(state.view(store));
+  };
+
+  store.subscribe(render);
+
+
+}//end of export
